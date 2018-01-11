526194bc58cbad959404642000f1ee26464f8d71
kanagala phani <kanagalaphani@kanagalas-MacBook-Air.local>
2018-01-11 15:55:20 +0100

    About page

---
+++ Views/about.php
@@ -0,0 +1,33 @@
+<html>
+<head>
+    <link rel="stylesheet" href="/Genhome/Styles/contact.css" />
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <style>
+h3 {
+    color: white;
+    margin-left: 40px;
+} 
+</style>
+</head>
+
+<body>
+  <div class="logo">
+        <img src="/Genhome/Images/logo.png" class="thelogo" alt="Logo">
+  </div>
+  <div class="container" align="center">  
+    <h1>About Us</h1>
+    <br>
+    
+    <h1 onmouseover="style.color='red'" onmouseout="style.color='white'">Delphine Coutin</h1>
+    <h1 onmouseover="style.color='red'" onmouseout="style.color='white'">Anthony Gomes</h1>
+    <h1 onmouseover="style.color='red'" onmouseout="style.color='white'">Phani kumar kanagala</h1>
+    <h1 onmouseover="style.color='red'" onmouseout="style.color='white'">Pierre Mecchia</h1>
+    <h1 onmouseover="style.color='red'" onmouseout="style.color='white'">Morgane Thivet</h1>
+    <h1 onmouseover="style.color='red'" onmouseout="style.color='white'">Alexandre Van Wesel</h1>
+    <h1 onmouseover="style.color='red'" onmouseout="style.color='white'">Rahul Yengal</h1>
+    <br>
+ 	<p class="back">Back to <a class="index" href="/Genhome/index.php">Genhome</a></p>
+  </div>
+</body>
+
+</html>
