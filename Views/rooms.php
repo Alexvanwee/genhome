fcb6c811a0cf64c4924c0f98088194384f7291e5
AnGomes <gomes.anthony94@gmail.com>
2018-01-25 16:42:25 +0100

    small fix

--- Views/rooms.php
+++ Views/rooms.php
@@ -8,11 +8,11 @@
     <title>My Rooms</title>
     <link rel="stylesheet" type="text/css" href="/Genhome/Styles/rooms.css"/>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <?php 
+   <!--  <?php 
         include_once  $_SERVER['DOCUMENT_ROOT'].'/Genhome/Models/get_user_name.php';
         $user_name = get_user_name($_SESSION["login"]);
         $user_name = $user_name[0];
-    ?>
+    ?> -->
 </head>
   
 
@@ -31,9 +31,9 @@
   		<div class="buttons">
             <button class="plus" type="button" formnovalidate="formnovalidate" onclick="location.href='addRooms.php'"><img src="/Genhome/Images/plus.png" alt="add" /></button> 
         </div>
-        <p id="member">
+       <!--  <p id="member">
             <?php include_once  $_SERVER['DOCUMENT_ROOT'].'/Genhome/Controllers/c_show_rooms.php';?>
-          </p>
+          </p> -->
  	</div>
     
     <input type="submit" value="Submit"/>
